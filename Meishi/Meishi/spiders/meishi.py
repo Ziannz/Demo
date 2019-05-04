de5426941e400405680d4f0cda179293cf64a8bd
xj <c88s130109@163.com>
2019-05-04 20:36:22 +0800

    first

---
+++ Meishi/Meishi/spiders/meishi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class MeishiSpider(scrapy.Spider):
+    name = 'meishi'
+    allowed_domains = ['meituan.com']
+    start_urls = ['http://meituan.com/']
+
+    def parse(self, response):
+        pass
